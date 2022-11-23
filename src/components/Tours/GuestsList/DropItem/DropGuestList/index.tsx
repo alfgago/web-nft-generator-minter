44d3fc2b8c93c5c79ee0fa04b80909463b74dd38
4ndreyMS <melendezzsequeira@gmail.com>
2022-11-23 14:48:40 -0600

    working on the guest form

--- src/components/Tours/GuestsList/DropItem/DropGuestList/index.tsx
+++ src/components/Tours/GuestsList/DropItem/DropGuestList/index.tsx
@@ -1,13 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
 import AddButton from "@/components/Common/AddButton"
 import Button from "@/components/Common/Button"
 import ItemPagination from "@/components/Common/ItemPagination"
+import Modal from "@/components/Common/Modal"
+import NewGuestForm from "@/components/Tours/NewGuestForm"
 
 import GuestItem from "../GuestItem"
 
 import { DropGuestListStyles } from "./DropGuestListStyles"
 const DropGuestList = ({ data }: any) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <DropGuestListStyles>
       <ItemPagination
@@ -29,8 +33,13 @@ const DropGuestList = ({ data }: any) => {
           fontSize={16}
           label="Export guest list"
         />
-        <AddButton label="Add name" />
+        <AddButton label="Add name" action={() => setIsOpen(!isOpen)} />
       </div>
+      {isOpen && (
+        <Modal setIsOpen={setIsOpen} title="Add guests">
+          <NewGuestForm />
+        </Modal>
+      )}
     </DropGuestListStyles>
   )
 }
