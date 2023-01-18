1dfcc2b52e38eba8030a7ead362303245a67a086
Andrey Meléndez <melendezzsequeira@gmail.com>
2023-01-18 17:38:12 -0600

    working on the road map section

--- src/components/About/RoadMap/RoadMapStyles.jsx
+++ src/components/About/RoadMap/RoadMapStyles.jsx
@@ -3,19 +3,70 @@ import styled from "styled-components"
 import { COLORS, DEVICE } from "@/styles/variables"
 
 export const RoadMapStyles = styled.section`
-  .cols-cont {
-    display: flex;
+  padding: 0;
+  display: flex;
+
+  .col-1,
+  .col-2 {
+    width: 50%;
+    position: relative;
   }
 
-  .column {
-    width: 50%;
+  .content {
     display: flex;
+    justify-content: center;
+    align-items: center;
     flex-direction: column;
+    padding-top: ${85 / 16}rem;
+
+    h2 {
+      padding-bottom: 76px;
+    }
+
+    .description {
+      max-width: ${444 / 16}rem;
+    }
+
+    .spacer {
+      color: transparent;
+    }
   }
+
   .col-1 {
-    align-items: flex-start;
+    &:before {
+      content: "";
+      z-index: -1;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(
+        94.68deg,
+        #f9e9ff 3.08%,
+        rgba(255, 255, 255, 0) 88.15%,
+        rgba(255, 255, 255, 0) 88.15%
+      );
+      transform-origin: center;
+    }
   }
+
   .col-2 {
-    align-items: flex-end;
+    color: ${COLORS.white};
+
+    &:before {
+      content: "";
+      z-index: -1;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(90.16deg, #000000 0.12%, #37648d 100%);
+      transform-origin: center;
+    }
+
+    .content > div {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      text-align: end;
+    }
   }
 `
--- src/components/About/RoadMap/index.tsx
+++ src/components/About/RoadMap/index.tsx
@@ -5,15 +5,34 @@ import { RoadMapStyles } from "./RoadMapStyles"
 const RoadMap = () => {
   return (
     <RoadMapStyles>
-      <div className="cols-cont content">
-        <div className="column col-1">
-          <h2>Roadmap</h2>
-          <h3>title</h3>
-          <p>description</p>
+      <div className="col-1">
+        <div className="content">
+          <div>
+            <h2>Roadmap</h2>
+            <h3>date</h3>
+            <h3>title</h3>
+            <p className="description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Architecto sed tenetur iure, atque, quaerat nemo odit vitae quod
+              dicta repudiandae illum, laborum doloremque cumque quis error
+              temporibus impedit eaque commodi.
+            </p>
+          </div>
         </div>
-        <div className="column col-2">
-          <h3>title</h3>
-          <p>description</p>
+      </div>
+      <div className="col-2">
+        <div className="content">
+          <div>
+            <h2 className="spacer">spacer</h2>
+            <h3>date</h3>
+            <h3>title</h3>
+            <p className="description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Architecto sed tenetur iure, atque, quaerat nemo odit vitae quod
+              dicta repudiandae illum, laborum doloremque cumque quis error
+              temporibus impedit eaque commodi.
+            </p>
+          </div>
         </div>
       </div>
     </RoadMapStyles>
