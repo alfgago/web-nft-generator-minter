e4865c36879df8430f987b9ba6adf212ae65e323
Andrey Meléndez <melendezzsequeira@gmail.com>
2023-02-02 16:06:02 -0600

    adding the logic by the row team members

--- src/components/About/TeamCard/index.tsx
+++ src/components/About/TeamCard/index.tsx
@@ -3,13 +3,13 @@ import React from "react"
 import { TeamCardStyles } from "./TeamCardStyles"
 const TeamCard = ({ cardData }: any) => {
   //   console.log(cardData.image.data.attributes.url)
-  const image = cardData.image.data.attributes.url
+  // const image = cardData.image.data.attributes.url
   return (
     <TeamCardStyles>
-      <img src={image} alt="" />
+      <img src={cardData.image} alt="" />
 
       <p>{cardData.name}</p>
-      <p>{cardData.Role}</p>
+      <p>{cardData.role}</p>
     </TeamCardStyles>
   )
 }
