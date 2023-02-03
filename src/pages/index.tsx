edb55d07400d4a43de520a8abe28252cc5cd20ac
Alfredo Gago <alfredo@5e.cr>
2023-02-02 18:39:18 -0600

    try fix runtime issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,29 +9,8 @@ const Index = ({ page, passes }: any) => {
       <Head>
         <title>Home - PlusOne</title>
       </Head>
-      <Home page={page.data} passes={passes.data} />
     </>
   )
-}
-
-export const getStaticProps = async () => {
-  const apiURL = process.env.API_URL ?? "http://localhost:1337/"
-  const token = process.env.API_TOKEN
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`
-
-  const postResponse = await axios.get(`${apiURL}/api/homepage?populate=deep,4`)
-
-  const passesResponse = await axios.get(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/api/passes?populate=*`
-  )
-
-  return {
-    props: {
-      page: postResponse.data,
-      passes: passesResponse.data,
-    },
-    revalidate: 30,
-  }
 }
 
 export default Index
