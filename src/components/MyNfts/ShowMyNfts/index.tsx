9921c2959329c12dbe8b0002d07e6b19275ddc6a
Andrey Meléndez <melendezzsequeira@gmail.com>
2023-02-24 15:39:46 -0600

    adding the event listener on the screen with[

--- src/components/MyNfts/ShowMyNfts/index.tsx
+++ src/components/MyNfts/ShowMyNfts/index.tsx
@@ -9,6 +9,18 @@ import { ShowMyNftStyles } from "./ShowMyNftStyles"
 const ShowMyNfts = ({ items }: any) => {
   // console.log("items")
   // console.log(items)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenWidth(window.innerWidth)
+      // do something every time the screen changes
+      console.log("Screen size changed")
+    }
+
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
   return (
     <ShowMyNftStyles className="my-nfts">
@@ -16,7 +28,7 @@ const ShowMyNfts = ({ items }: any) => {
         <div className="list">
           {items.length > 0 ? (
             <ItemPagination
-              itemsPerPage={3}
+              itemsPerPage={screenWidth > 1080 ? 3 : 2}
               values={items}
               render={(itemsVals: any) => {
                 return (
