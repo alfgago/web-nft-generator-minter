ea1746975112f2c5b2f0298ad456c4224bd7550c
Andrey Meléndez <melendezzsequeira@gmail.com>
2023-02-23 18:15:30 -0600

    workin on the lottery

---
+++ src/pages/api/shows/wallet-lottery.ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import axios from "axios"
+import NodeCache from "node-cache"
+const cache = new NodeCache({ stdTTL: 30 }) // cache for 30 seconds
+
+const fetchData = async ({
+  page = 1,
+  limit = 10,
+  passType = false,
+  nftImage = false,
+}: any) => {
+  const apiURL = process.env.API_URL ?? "http://localhost:1337/"
+  const token = process.env.API_TOKEN
+
+  const cacheKey = `shows_${page}_${limit}_${passType}`
+  const cached = cache.get(cacheKey)
+  if (cached) {
+    return cached
+  }
+
+  const params = {
+    "pagination[page]": page,
+    "pagination[pageSize]": limit,
+    populate: "passes",
+  }
+  if (passType) {
+    // @ts-ignore
+    params["filters[passes][pass_type][$eq]"] = passType
+  }
+
+  if (nftImage) {
+    console.log(nftImage)
+    // @ts-ignore
+    params["filters[artist][passes][nfts][image_url][$eq]"] = nftImage
+  }
+
+  const response = await axios.get(`${apiURL}/api/events`, {
+    params: params,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  cache.set(cacheKey, response.data)
+  console.log(response.data)
+  return response.data
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const data = await fetchData(req.query)
+    res.status(200).json(data)
+  } catch (e) {
+    console.log(e)
+    res.status(400).send({ err: "There was an error fetching the data" })
+  }
+}
