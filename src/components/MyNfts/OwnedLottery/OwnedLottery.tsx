82f4d42692ae9ba80011209568a08c787dee5e37
Andrey Meléndez <melendezzsequeira@gmail.com>
2023-03-02 12:13:41 -0600

    removing logs

--- src/components/MyNfts/OwnedLottery/OwnedLottery.tsx
+++ src/components/MyNfts/OwnedLottery/OwnedLottery.tsx
@@ -8,129 +8,6 @@ import Index from "@/pages"
 import OwnedItem from "./OwnedItem"
 import { OwnedLotteryStyles } from "./OwnedLotteryStyles"
 
-const lotteryItemsList = [
-  {
-    id: 1,
-    origin: `upcoming`,
-    chanceOfWinning: 50,
-    nft: {
-      id: "Bull-1",
-      name: "Bull Island",
-      info: "Illander bull",
-      price: 500,
-      amount: 10,
-      image: "/assets/img/myNft.jpg",
-      tourName: "Illander tour",
-      stake: {
-        staked: 9,
-        total: 10,
-      },
-      property: {
-        owned: 20,
-        total: 90,
-      },
-    },
-    event: {
-      id: 1,
-      venue: "Nashville Stadium",
-      state: "Georgia",
-      city: "Nashville",
-      date: "june 19",
-      timeLeftCalc: "5 hrs 30 min 21 sec",
-    },
-  },
-  {
-    id: 2,
-    origin: `active`,
-    chanceOfWinning: 60,
-    nft: {
-      id: "bull-2",
-      name: "Green Island bull",
-      info: "The Green Island bull",
-      price: 200,
-      amount: 100,
-      image: "/assets/img/myNftBlue.jpg",
-      tourName: "Green Island",
-      stake: {
-        staked: 10,
-        total: 20,
-      },
-      property: {
-        owned: 25,
-        total: 90,
-      },
-    },
-    event: {
-      id: 2,
-      venue: "Kansas Stadium",
-      state: "Kansas",
-      city: "Delaware",
-      date: "September 11",
-      timeLeftCalc: "8 days",
-    },
-  },
-  {
-    id: 3,
-    origin: `upcoming`,
-    chanceOfWinning: 45,
-    nft: {
-      id: "bull-3",
-      name: "Dragon Bull",
-      info: "The dragon concert",
-      price: 700,
-      amount: 35,
-      image: "/assets/img/myNft.jpg",
-      tourName: "Dragon Bull Tour",
-      stake: {
-        staked: 8,
-        total: 15,
-      },
-      property: {
-        owned: 40,
-        total: 80,
-      },
-    },
-    event: {
-      id: 3,
-      venue: "Georgia Stadium",
-      state: "Georgia",
-      city: "Nashville",
-      date: "june 18",
-      timeLeftCalc: "1 week",
-    },
-  },
-  {
-    id: 4,
-    origin: `active`,
-    chanceOfWinning: 50,
-    nft: {
-      id: "bull-4",
-      name: "Ocean Concer",
-      info: "Concert in the Ocean",
-      price: 1500,
-      amount: 250,
-      image: "/assets/img/myNftBlue.jpg",
-      tourName: "Ocean Concer Tour",
-      stake: {
-        staked: 25,
-        total: 50,
-      },
-      property: {
-        owned: 70,
-        total: 15,
-      },
-    },
-    event: {
-      id: 4,
-      venue: "California Stadium",
-      state: "California",
-      city: "San Francisco",
-      date: "April 11",
-      timeLeftCalc: "2 months",
-    },
-  },
-]
-
 const OwnedLottery = () => {
   const [lotteryNfts, setLotteryNfts] = useState([])
   const [filter, setFilter] = useState("")
@@ -169,7 +46,6 @@ const OwnedLottery = () => {
           `/api/artists/wallet-lottery?nftImage=${element.event}`
         )
 
-        // console.log(data.data[index].attributes.events)
         setArtistData(data.data[index].attributes.events.data)
       })
     } catch (error) {}
@@ -188,6 +64,7 @@ const OwnedLottery = () => {
     return { hours: hours, minutes: minutes, seconds: seconds }
   }
 
+  // filter function
   useEffect(() => {
     let filteredList = artistData
     // const remainingTime = getTime(new Date(el.attributes.date))
@@ -205,8 +82,6 @@ const OwnedLottery = () => {
     }
     setLotteryNfts(filteredList)
   }, [filter, artistData])
-
-  console.log(lotteryNfts)
 
   return (
     <OwnedLotteryStyles>
