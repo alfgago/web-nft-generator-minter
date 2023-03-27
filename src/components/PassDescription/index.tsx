6cb14f50a37ad4d7082d2364ddd18d8b76b46511
Alfredo Gago <alfredo@5e.cr>
2023-03-27 08:04:06 -0600

    add changes

--- src/components/PassDescription/index.tsx
+++ src/components/PassDescription/index.tsx
@@ -1,8 +1,8 @@
 const PassDescription = ({ pass }: any) => {
   const passType = pass.attributes.pass_type
-  const artistName = pass.attributes.artist.data.attributes.name
-  const venue = pass.attributes.event.data.attributes.venue
-  const date = pass.attributes.event.data.attributes.date
+  const artistName = pass.attributes.artist?.data?.attributes?.name ?? ""
+  const venue = pass.attributes.event?.data?.attributes?.venue ?? ""
+  const date = pass.attributes.event?.data?.attributes?.date ?? ""
 
   const dateFormat = (value: any) => {
     const date = new Date(value)
