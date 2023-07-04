b06fbce9de9fa9b4f26130958a1a7d6cc110dd79
Alfredo Gago <alfredo@5e.cr>
2023-07-04 15:59:11 -0600

    fix images

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,10 +13,7 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - favicon.ico (favicon file)
      */
-    "/((?!api|_next/static|favicon.ico|assets|under-development.svg).*)",
+    "/((?!api|_next/static|favicon.ico|assets|uploads|aws|ipfs).*)",
   ],
 }
