0b47ad64b9bf723914782bb3b790d867e1bcb3fd
Alfredo Gago <alfredo@5e.cr>
2023-09-13 04:19:51 -0600

    Fixes

---
+++ src/pages/api/nfts/token-uri.ts
@@ -0,0 +1,78 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import axios from "axios"
+import NodeCache from "node-cache"
+const cache = new NodeCache({ stdTTL: 10 }) // cache for 10 seconds
+
+import { setTokenURI, TokenUriParams } from "@/utils/SmartContracts/setTokenUri"
+
+import "dotenv/config"
+
+type ErrResponseBody = {
+  err: string
+}
+
+type AirdropResponseBody =
+  | {
+      transactionHash: string
+    }
+  | ErrResponseBody
+
+const isMinted = async (contractAddress: string, order: number) => {
+  const alchemyToken = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+  const alchemyDomain =
+    process.env.NEXT_PUBLIC_NETWORK == "goerli"
+      ? "https://eth-goerli.g.alchemy.com"
+      : "https://polygon-mainnet.g.alchemy.com"
+
+  const cacheKey = `getNFTsForCollection_${contractAddress}`
+
+  let getNFTsForCollection = cache.get(cacheKey)
+
+  if (!getNFTsForCollection) {
+    getNFTsForCollection = []
+    try {
+      const response = await axios.get(
+        `${alchemyDomain}/nft/v2/${alchemyToken}/getNFTsForCollection?contractAddress=${contractAddress}&withMetadata=true`
+      )
+      getNFTsForCollection = response?.data?.nfts ?? []
+    } catch (e) {
+      console.log(e)
+    }
+    cache.set(cacheKey, getNFTsForCollection)
+  }
+
+  console.log(getNFTsForCollection[order - 1])
+  const gateway = getNFTsForCollection[order - 1]?.tokenUri?.gateway ?? ""
+  return gateway
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<AirdropResponseBody>
+) {
+  try {
+    const { contractAddress, network, tokenId, metadataCid, nftId } = req.body
+    const hash = await isMinted(contractAddress, parseInt(tokenId))
+    if (!hash.length) {
+      axios.post(process.env.NEXT_PUBLIC_DOMAIN + "/api/nfts/update-status", {
+        id: nftId,
+        mint_order: tokenId,
+      })
+
+      const transactionHash = await setTokenURI({
+        contractAddress,
+        network,
+        tokenId,
+        metadataCid,
+      })
+
+      console.log(transactionHash)
+
+      res.status(200).json({ transactionHash: transactionHash })
+    }
+    return res.status(200).json({ transactionHash: hash })
+  } catch (e) {
+    console.log(e)
+    res.status(400).send({ err: "Bad Request:" + e })
+  }
+}
--- src/pages/pass/[slug].tsx
+++ src/pages/pass/[slug].tsx
@@ -1,16 +1,76 @@
+import { useEffect, useState } from "react"
 import dynamic from "next/dynamic"
 import Head from "next/head"
+import { useRouter } from "next/router"
 import axios from "axios"
+import { useInterval } from "usehooks-ts"
 
 import cleanUrl from "@/utils/cleanUrl"
 
 const SinglePass = dynamic(() => import("@/components/SinglePass"))
+
+const checkTransactionStatus = async (transactionId, metadataCid, nftId) => {
+  try {
+    const { data } = await axios.get(
+      "https://withpaper.com/api/v1/transaction-status/" + transactionId
+    )
+    const claimedTokens = data.result.claimedTokens
+    const tokenId = claimedTokens.tokens[0].tokenId
+    const collectionAddress = claimedTokens.collectionAddress
+    console.log("claimedTokens")
+    console.log(claimedTokens)
+
+    const res = await axios.post(
+      process.env.NEXT_PUBLIC_DOMAIN + "/api/nfts/token-uri",
+      {
+        contractAddress: collectionAddress,
+        network: process.env.NEXT_PUBLIC_NETWORK ?? "goerli",
+        tokenId,
+        metadataCid,
+        nftId,
+      }
+    )
+
+    return res.data
+  } catch (e) {
+    console.log(e)
+    return false
+  }
+}
 
 const PassPage = ({ pass }: any) => {
   const title = pass.attributes.collection_name
   const image = cleanUrl(pass.attributes.preview_image_url)
   const ogTitle = title + " - PlusOne"
   const bio = ""
+
+  // Get the router object
+  const router = useRouter()
+
+  useInterval(async () => {
+    // @ts-ignore
+    if (!window.transactionStatus && !window.sending) {
+      // @ts-ignore
+      window.sending = true
+      const transactionQuery = router.query
+      if (!transactionQuery) return
+
+      const { transactionId, metadataCid, nftId } = transactionQuery
+
+      if (!transactionId) return
+
+      const status = await checkTransactionStatus(
+        transactionId,
+        metadataCid,
+        nftId
+      )
+      // @ts-ignore
+      window.transactionStatus = status
+      // @ts-ignore
+      window.sending = false
+      console.log(status)
+    }
+  }, 3000)
 
   return (
     <>
@@ -30,11 +90,6 @@ export const getServerSideProps = async ({ query }: any) => {
   const apiURL = process.env.NEXT_PUBLIC_STRAPI_URL ?? "http://localhost:1337/"
   const token = process.env.API_TOKEN
 
-  setTimeout(async () => {
-    const status = await checkTransactionStatus(query)
-    console.log(status)
-  }, 5000)
-
   const response = await axios.get(`${apiURL}/api/passes`, {
     params: {
       populate: "artist.banner,event,tour,collection_preview_image",
@@ -52,33 +107,6 @@ export const getServerSideProps = async ({ query }: any) => {
       },
     }
   }
-}
-
-const checkTransactionStatus = async (query: any) => {
-  if (!query.transactionId) return
-
-  const transactionId = query.transactionId
-  const metadataCid = query.metadataCid
-  const nftId = query.nftId
-  const { data } = await axios.get(
-    "https://withpaper.com/api/v1/transaction-status/" + transactionId
-  )
-  const claimedTokens = data.result.claimedTokens
-  const tokenId = claimedTokens.tokens[0].tokenId
-  const collectionAddress = claimedTokens.collectionAddress
-
-  const res = await axios.post(
-    process.env.NEXT_PUBLIC_DOMAIN + "/api/nfts/set-token-uri",
-    {
-      contractAddress: collectionAddress,
-      network: process.env.NEXT_PUBLIC_NETWORK ?? "goerli",
-      tokenId,
-      metadataCid,
-      nftId,
-    }
-  )
-
-  return res.data
 }
 
 export default PassPage
