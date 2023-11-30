eaf18a7c92bc37f0cd5349f2c1dfba077287417d
Alfredo Gago <alfredo@5e.cr>
2023-11-29 18:19:05 -0600

    change pass

--- src/pages/api/password-protect.ts
+++ src/pages/api/password-protect.ts
@@ -10,7 +10,7 @@ export default async function handler(
   }
   const password = req.body.password
   if (process.env.PASSWORD_PROTECT === password) {
-    const cookie = serialize("p1login", "true", {
+    const cookie = serialize("P!U$0N3", "true", {
       path: "/",
       httpOnly: true,
     })
